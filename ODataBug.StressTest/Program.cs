e2c235f30692e670986c0c8b2f0619fd61b7f668
Matt Kleinschafer <matt.kleinschafer@karbonhq.com>
2023-11-07 09:20:31 +1100

    Add a bit more detail to console output

--- ODataBug.StressTest/Program.cs
+++ ODataBug.StressTest/Program.cs
@@ -1,32 +1,58 @@
-﻿Parallel.Invoke(
+﻿var isCancelling = false;
+
+Console.CancelKeyPress += (_, _) => { isCancelling = true; };
+
+Parallel.Invoke(
     () =>
     {
         using var client = BuildClient();
 
-        while (true)
+        const string url = "/v3/Contact?$expand=BusinessCards";
+
+        var successCount = 0;
+        var failedCount = 0;
+
+        while (!isCancelling)
         {
-            var response = client.GetAsync("/v3/Contact?$expand=BusinessCards").Result;
+            var response = client.GetAsync(url).Result;
+
+            if (!response.IsSuccessStatusCode)
+                Interlocked.Increment(ref failedCount);
+            else
+                Interlocked.Increment(ref successCount);
 
             if (!response.IsSuccessStatusCode)
             {
-                Console.WriteLine(response.StatusCode);
                 Console.WriteLine(response.Content.ReadAsStringAsync().Result);
             }
+            
+            Console.WriteLine($"There were {successCount} successful responses and {failedCount} failed responses for {url}");
         }
     },
     () =>
     {
         using var client = BuildClient();
 
-        while (true)
+        const string url = "/v3/Organisation?$expand=Contacts";
+
+        var successCount = 0;
+        var failedCount = 0;
+
+        while (!isCancelling)
         {
-            var response = client.GetAsync("/v3/Organisation?$expand=Contacts").Result;
+            var response = client.GetAsync(url).Result;
+
+            if (!response.IsSuccessStatusCode)
+                Interlocked.Increment(ref failedCount);
+            else
+                Interlocked.Increment(ref successCount);
 
             if (!response.IsSuccessStatusCode)
             {
-                Console.WriteLine(response.StatusCode);
                 Console.WriteLine(response.Content.ReadAsStringAsync().Result);
             }
+            
+            Console.WriteLine($"There were {successCount} successful responses and {failedCount} failed responses for {url}");
         }
     }
 );
